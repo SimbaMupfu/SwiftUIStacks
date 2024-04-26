8704d8c6e46e5a6235696a2222daf559c64bab60
simba_mupfu <scmupfururirwa@gmail.com>
2024-04-26 04:39:34 +0200

    Create the Basic Plan View.

--- SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/ContentView.swift
@@ -9,6 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack {
+            HeaderView()
+        
+            HStack{
+                VStack{
+                    Text("Basic")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                    Text("$9")
+                        .font(.system(size: 40, weight: .heavy, design: .rounded))
+                        .foregroundColor(.white)
+                    Text("per month")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                }
+                .padding(40)
+                .background(Color.blue)
+                .cornerRadius(10)
+                VStack{
+                    Text("Pro")
+                    Text("$19")
+                    Text("per month")
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
         VStack(alignment: .leading, spacing: 2){
             Text("Choose")
                 .font(.system(.largeTitle, design: .rounded))
@@ -17,11 +54,5 @@ struct ContentView: View {
                 .font(.system(.largeTitle, design: .rounded))
                 .fontWeight(.black)
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
